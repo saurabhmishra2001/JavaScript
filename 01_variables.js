81f19a7baafc6377ae4e3c0ab205b411b8822523
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-10 20:53:06 +0530

    01

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+console.log("Welcome to this application");
+let a=56;
+console.log(a);
+let b=56;
+console.log(a+b);
+let c="Hello world!";
+console.log(c+"\n");
---
+++ 1.js
@@ -0,0 +1,4 @@
+console.log("Starting");
+a=5,b=5,c=5,d=5,e=5;
+sum= a+b+c+d+e;
+console.log("Sum: "+sum);

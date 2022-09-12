29911a7c718e4b9591d1204c56f7f706842b3bec
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-12 13:46:34 +0530

    added

---
+++ 06_Expression.js
@@ -0,0 +1,27 @@
+console.log("Operators in javascript");
+let a=23;
+let b=3;
+console.log("a+b is",a+b);
+console.log("a-b is",a-b);
+console.log("a*b is",a*b);
+console.log("a/b is",a/b);
+console.log("a%b is",a%b);
+console.log("a**b is",a**b);
+console.log("a++ is",a++);
+console.log("a-- is",a--);
+console.log("++a",++a);
+console.log("--a",--a);
+
+//assignment operators
+let assignment = 4;
+console.log("assignment is",assignment);
+assignment += 5;
+console.log("assignment += 5 is",assignment);
+assignment -= 5;
+console.log("assignment -= 5 is",assignment);
+assignment *= 5;
+console.log("assignment *= 5 is",assignment);
+assignment /= 5;
+console.log("assignment /= 5 is",assignment);
+assignment %= 5;
+console.log("assignment %= 560 is",assignment);
